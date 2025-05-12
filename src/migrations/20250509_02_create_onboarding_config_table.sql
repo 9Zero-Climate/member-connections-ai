9e723ee001434581b58b14b980e604490b31ebad
jason-curtis <jason@optionzero.co>
2025-05-12 14:23:39 -0700

    fix fixture in db migration

--- src/migrations/20250509_02_create_onboarding_config_table.sql
+++ src/migrations/20250509_02_create_onboarding_config_table.sql
@@ -11,35 +11,37 @@ INSERT INTO onboarding_config (
     )
 VALUES (
         'Seattle',
-        ARRAY ['U073CUASRSR', 'U07QU1QCX52'],
         -- Lowell, Laura Knaub
-        'We' re excited to have you ! Here are some tips: -
-        Join #introductions (introduce yourself here!)
-        - Also
-        join #sea-announcements, #sea-water-cooler, and #sea-whos-in (to see who's in the space today)
-        - Make a request in #ask.
-        Outside of slack: * Bookmark the members portal
-        and members directory on your browser * Bring your lunch on Wednesdays 12 -1 for Members Lunch,
-        on Thursday for the Lunch
-        and Learn * check out other events on the [calendar](
-            https: // www.linkedin.com / feed /
-            update / urn :li :activity :7259263131774824450 /
-        ).As always let us know if there 's anything we can do to support you!'
+        ARRAY ['U073CUASRSR', 'U07QU1QCX52'],
+        'Here are some tips:
+
+1️⃣ Join #introductions (introduce yourself here!)
+2️⃣ Also join #sea-announcements, #sea-water-cooler, and #sea-whos-in (to see who''s in the space today)
+3️⃣ Make a request in #ask.
+        
+Outside of slack:
+
+  •  Bookmark the <https://members.9zero.com/login|members portal> and <https://www.notion.so/9zero/13077f10821680f38eafcce7d3f72e17|members directory> on your browser
+  •  Bring your lunch on Wednesdays 12-1 for Members Lunch and on Thursdays for the Lunch and Learn
+  •  check out other events on the <https://www.linkedin.com/feed/update/urn:li:activity:7259263131774824450/|calendar>.
+
+As always let us know if there''s anything we can do to support you!'
     ),
     (
         'San Francisco',
-        ARRAY ['U08AG6NF90T', 'U07394PGKRC'],
         -- Daphne, Kirkland
-        'We''re excited to have you! Here are some tips:
+        ARRAY ['U08AG6NF90T', 'U07394PGKRC'],
+        'Here are some tips:
+
+1️⃣ Join #introductions (introduce yourself here!)
+2️⃣ Make a request in #ask
+3️⃣ Also join #sf-events, #sf-general (for everything else), #sf-whos-in (to see who''s in the space today), #sf-coffee-chats to be randomly matched for a coffee each week... etc etc. There are so many!
         
-        - Join #introductions (introduce yourself here!)
-        - Make a request in #ask
-        - Also join #sf-events, #sf-general (for everything else), #sf-whos-in (to see who''s in the space today), #sf-coffee-chats to be randomly matched for a coffee each week... etc etc. There are so many!
-        
-        Outside of slack:
-        * Bookmark the members portal and members directory on your browser
-        * Stop by the front desk to grab your 9Zero t-shirt and get your member polaroid taken! 📸
-        * Bring your lunch on Wednesdays 12-1 for Members Lunch, and check out other events on the [calendar](https://9zero.com/sf-subscribe-to-the-events-calendar).
-        
-        As always let us know if there''s anything we can do to support you!'
+Outside of slack:
+
+  •  Bookmark the <https://members.9zero.com/login|members portal> and <https://www.notion.so/9zero/13077f10821680f38eafcce7d3f72e17|members directory> on your browser
+  •  Stop by the front desk to grab your 9Zero t-shirt and get your member polaroid taken! 📸
+  •  Bring your lunch on Wednesdays 12-1 for Members Lunch, and check out other events on the <https://9zero.com/sf-subscribe-to-the-events-calendar|calendar>.
+
+As always let us know if there''s anything we can do to support you!'
     ) ON CONFLICT (location) DO NOTHING;
